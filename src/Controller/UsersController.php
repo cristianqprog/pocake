2c9921d1684d518870beac915083f1ced5e56dc4
cristianqprog <cristianqprog@hotmail.com>
2019-07-15 18:46:04 -0300

    creacion de view add y hash password

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -13,4 +13,17 @@ class UsersController extends AppController
 		$users = $this->Users->find('all');
 		$this->set('users',$users);
 	}
+
+	public function add (){
+	$user  =  $this -> Users -> newEntity ();
+	if ($this->request->is('post')) {
+		$user = $this->Users->patchEntity($user, $this->request->data);
+		 if ($this->Users->save($user)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+	}
+	$this->set('user', $user);
+	}
 }
